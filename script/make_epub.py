b17ffa01fccf4fbbf20a2973f3bf22a12be8ba6a
skywind3000 <skywind3000@163.com>
2024-05-24 17:08:06 +0800

    update files

--- script/make_epub.py
+++ script/make_epub.py
@@ -11,7 +11,7 @@
 import sys
 import time
 import os
-import ebooklib.epub
+import ebooklib.epub as epub
 import gonglue
 
 
@@ -22,74 +22,77 @@ class EpubBook (object):
 
     def __init__ (self):
         self.htmls = gonglue.list_html()
-        self.epub = ebooklib.epub.EpubBook()
+        self.book = epub.EpubBook()
 
     def init (self):
-        epub = self.epub
+        book = self.book
         uuid = 'GAMEGUIDE-LW-' + time.strftime('%Y%m%d%H%M%S')
-        epub.set_identifier(uuid)
-        epub.set_title('Game Guide')
-        epub.add_author('skywind')
-        epub.set_language('zh')
+        book.set_identifier(uuid)
+        book.set_title('Game Guide')
+        book.add_author('skywind')
+        book.set_language('zh')
+        book.toc = []
+        book.spine = []
 
     def add_html (self):
-        epub = self.epub
+        book = self.book
         if '*' in self.htmls:
             for fn in self.htmls['*']:
                 title = self.htmls['*'][fn]
                 srcname = f'html/{fn}'
                 print(srcname)
                 filename = os.path.join(gonglue.BUILD, srcname)
-                item = ebooklib.epub.EpubHtml(title=title, file_name=srcname)
+                item = epub.EpubHtml(title=title, file_name=srcname)
                 item.content = gonglue.read_file_content(filename)
-                epub.add_item(item)
-                epub.toc.append(item)
+                book.add_item(item)
+                book.toc.append(item)
+                book.spine.append(item)
         for dirname in self.htmls:
             count = 0
             if dirname == '*':
                 continue
-            section = [ebooklib.epub.Section(dirname), []]
+            section = [epub.Section(dirname), []]
             for fn in self.htmls[dirname]:
                 title = self.htmls[dirname][fn]
                 srcname = f'html/{dirname}/{fn}'
                 print(srcname)
                 filename = os.path.join(gonglue.BUILD, srcname)
-                item = ebooklib.epub.EpubHtml(title=title, file_name=srcname)
+                item = epub.EpubHtml(title=title, file_name=srcname)
                 item.content = gonglue.read_file_content(filename)
-                epub.add_item(item)
+                book.add_item(item)
+                book.spine.append(item)
                 section[1].append(item)
                 count += 1
-                if count > 5:
+                if count > 5 and 0:
                     break
             section[1] = tuple(section[1])
-            epub.toc.append(tuple(section))
+            book.toc.append(tuple(section))
         return 0
 
     def add_images (self):
-        epub = self.epub
+        book = self.book
         for root, dirs, files in os.walk(gonglue.IMAGES):
             for fn in files:
                 filename = os.path.join(root, fn)
                 srcname = os.path.relpath(filename, gonglue.IMAGES)
                 srcname = f'images/{srcname}'
                 mimetype = gonglue.guess_mimetype(filename)
-                item = ebooklib.epub.EpubItem(file_name=srcname, media_type=mimetype)
+                item = epub.EpubItem(file_name=srcname, media_type=mimetype)
                 with open(filename, 'rb') as f:
                     item.content = f.read()
-                epub.add_item(item)
+                book.add_item(item)
                 print('+', srcname, mimetype)
         return 0
 
     def build (self):
         print('building epub...')
-        epub = self.epub
+        book = self.book
         # toc: table of content (index menu)
-        epub.add_item(ebooklib.epub.EpubNcx())
+        book.add_item(epub.EpubNcx())
         # navi pages
-        # epub.add_item(ebooklib.epub.EpubNav())
-        epub.spine = ['nav'] + self.epub.toc
-        epub.add_item(ebooklib.epub.EpubCover())
-        ebooklib.epub.write_epub(f'{gonglue.BUILD}/GONGLUE.epub', epub, {})
+        # epub.add_item(epub.EpubNav())
+        book.add_item(epub.EpubCover())
+        epub.write_epub(f'{gonglue.BUILD}/GONGLUE.epub', book, {})
         return 0
 
 
