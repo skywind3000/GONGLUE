d7dd16a33fb210de4e7533be439d2a051f139b2a
skywind3000 <skywind3000@163.com>
2024-05-26 03:38:44 +0800

    1. 修改 python-markdown 输出结果里的外部链接，统一添加 target=_blank 2. 修复 iOS 下 epub 目录跳转失效问题，将所有 epub 内的章节挪至根目录。 3. 修改 epub 的章节内的 css/image 资源引用到位置，适配上面的移位。 4. 引入 BeautifulSoup4 的依赖（用于上面的修改）

--- script/make_epub.py
+++ script/make_epub.py
@@ -11,8 +11,36 @@
 import sys
 import time
 import os
+import bs4
 from ebooklib import epub
 import gonglue
+
+
+#----------------------------------------------------------------------
+# change image/css url: because epub requires all xhtml files in 
+# the root directory, so we need to change the image/css url
+#----------------------------------------------------------------------
+def patch_html(html):
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    for tag in soup.find_all('img'):
+        src = tag.get('src')
+        if src:
+            pos = src.find('/images/')
+            if pos >= 0:
+                tag['src'] = 'images/' + src[pos + 8:]
+    for tag in soup.find_all('link'):
+        href = tag.get('href')
+        if href:
+            pos = href.find('/images/')
+            if pos >= 0:
+                tag['href'] = 'images/' + href[pos + 8:]
+    for tag in soup.find_all('a'):
+        href = tag.get('href')
+        if href:
+            if href.startswith('http://') or href.startswith('https://'):
+                tag['target'] = '_blank'
+    html = str(soup)
+    return html
 
 
 #----------------------------------------------------------------------
@@ -28,9 +56,9 @@ class EpubBook (object):
         book = self.book
         uuid = 'GAMEGUIDE-LW-' + time.strftime('%Y%m%d%H%M%S')
         book.set_identifier(uuid)
-        book.set_title('Game Guide')
+        book.set_title('游戏攻略合集')
+        book.set_language('zh-CN')
         book.add_author('skywind')
-        book.set_language('zh')
         book.toc = []
         book.spine = []
         self.css_epub = None
@@ -39,40 +67,61 @@ class EpubBook (object):
         book.set_cover('cover.jpg', open(image, 'rb').read())
         book.spine = ['cover', 'nav']
 
-    def add_html (self):
+    def add_html (self, debug = False):
         book = self.book
+        mode = 0
+        index = 0
+        category = 0
         if '*' in self.htmls:
             for fn in self.htmls['*']:
                 title = self.htmls['*'][fn]
                 srcname = f'html/{fn}'
+                dstname = f'html/{index}.xhtml'
+                dstname = f'c{index}.xhtml'
+                index += 1
                 print(srcname)
                 filename = os.path.join(gonglue.BUILD, srcname)
-                item = epub.EpubHtml(title=title, file_name=srcname)
+                item = epub.EpubHtml(title=title, file_name=dstname)
                 item.content = gonglue.read_file_content(filename)
+                item.content = patch_html(item.content)
                 item.media_type = 'application/xhtml+xml'
                 item.add_item(self.css_epub)
                 book.add_item(item)
-                book.toc.append(item)
                 book.spine.append(item)
+                if mode == 0:
+                    book.toc.append(item)
+                else:
+                    link = epub.Link(srcname, title, srcname)
+                    book.toc.append(link)
         for dirname in self.htmls:
             count = 0
             if dirname == '*':
                 continue
+            entry = f'{category}'
+            category += 1
             section = [epub.Section(dirname), []]
             for fn in self.htmls[dirname]:
                 title = self.htmls[dirname][fn]
                 srcname = f'html/{dirname}/{fn}'
+                dstname = f'html/{entry}/{index}.xhtml'
+                dstname = f'c{index}.xhtml'
+                index += 1
                 print(srcname)
                 filename = os.path.join(gonglue.BUILD, srcname)
-                item = epub.EpubHtml(title=title, file_name=srcname)
+                item = epub.EpubHtml(title=title, file_name=dstname)
                 item.content = gonglue.read_file_content(filename)
+                item.content = patch_html(item.content)
                 item.media_type = 'application/xhtml+xml'
                 item.add_item(self.css_epub)
                 book.add_item(item)
                 book.spine.append(item)
-                section[1].append(item)
+                if mode == 0:
+                    section[1].append(item)
+                else:
+                    link = epub.Link(srcname, title, srcname)
+                    section[1].append(link)
                 count += 1
-                if count > 5 and 0:
+                if count > 5 and debug:
                     break
             book.toc.append(section)
         return 0
@@ -82,12 +131,19 @@ class EpubBook (object):
         for root, dirs, files in os.walk(gonglue.IMAGES):
             for fn in files:
                 filename = os.path.join(root, fn)
+                extname = os.path.splitext(fn)[1].lower()
+                if extname == '.md':
+                    continue
+                if fn == 'style.css':
+                    continue
                 srcname = os.path.relpath(filename, gonglue.IMAGES)
                 srcname = f'images/{srcname}'
                 mimetype = gonglue.guess_mimetype(filename)
                 item = epub.EpubItem(file_name=srcname, media_type=mimetype)
                 with open(filename, 'rb') as f:
                     item.content = f.read()
+                if extname == '.css':
+                    item.content += b'\n\n'
                 book.add_item(item)
                 print('+', srcname, mimetype)
         name = os.path.join(gonglue.PROJECT, 'images/style_epub.css')
@@ -109,7 +165,11 @@ class EpubBook (object):
         book.add_item(epub.EpubNcx())
         # navi pages
         book.add_item(epub.EpubNav())
-        epub.write_epub(f'{gonglue.BUILD}/GONGLUE.epub', book, {})
+        opts = {}
+        opts['epub3_landmark'] = False
+        opts['epub3_pages'] = False
+        opts['epub3_pages'] = False
+        epub.write_epub(f'{gonglue.BUILD}/GONGLUE.epub', book, opts)
         return 0
 
 
@@ -123,7 +183,7 @@ if __name__ == '__main__':
         book = EpubBook()
         book.init()
         book.add_images()
-        book.add_html()
+        book.add_html(debug = 0)
         book.build()
         return 0
     test1()
